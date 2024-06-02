4afbf41ffddd2f1668ee6405be2ae451a3a7ef50
Daniel Rosel <daniel@alves.world>
2024-06-02 15:38:12 +0200

    refactored

--- fabric.el
+++ fabric.el
@@ -1,5 +1,37 @@
+;;; fabric.el --- A Spacemacs package integrates with Fabric AI to enhance workflows with pattern application and retrieval functions. -*- lexical-binding: t -*-
+
+;; Author: Daniel Rosel
+;; Maintainer: Daniel Rosel
+;; Version: 0.1
+;; Package-Requires: ()
+;; Homepage: https://github.com/velocitatem/fabric.el
+;; Keywords: ai fabric spacemacs emacs LLMs
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
 ;; A spacemacs package to use fabric https://github.com/danielmiessler/fabric
-;;  fabric is an open-source framework for augmenting humans using AI. It provides a modular framework for solving specific problems using a crowdsourced set of AI prompts that can be used anywhere.
+;; fabric is an open-source framework for augmenting humans using AI. It provides a modular framework for solving specific problems using a crowdsourced set of AI prompts that can be used anywhere.
+
+;;; Code:
+
+(provide 'fabric)
 
 
 (defun fabric-get-patterns ()
@@ -74,3 +106,5 @@
 (spacemacs/set-leader-keys "aib" 'fabric-run-pattern-on-buffer)
 (spacemacs/set-leader-keys "aiR" 'fabric-run-pattern-on-region)
 (spacemacs/set-leader-keys "aiP" 'fabric-get-patterns)
+
+;;; fabric.el ends here
